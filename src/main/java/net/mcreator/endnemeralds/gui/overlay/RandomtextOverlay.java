cfe1d4c03eda814360c4f2966269880ec039344f
buddyr4935 <user@LAPTOP-BFL4CMRL>
2021-07-10 19:08:01 +1000

    add something anoying

---
+++ src/main/java/net/mcreator/endnemeralds/gui/overlay/RandomtextOverlay.java
@@ -0,0 +1,45 @@
+
+package net.mcreator.endnemeralds.gui.overlay;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.endnemeralds.EndNEmeraldsModElements;
+
+@EndNEmeraldsModElements.ModElement.Tag
+public class RandomtextOverlay extends EndNEmeraldsModElements.ModElement {
+	public RandomtextOverlay(EndNEmeraldsModElements instance) {
+		super(instance, 35);
+	}
+
+	@Override
+	public void initElements() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public void eventHandler(RenderGameOverlayEvent event) {
+		if (!event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.HELMET) {
+			int posX = (event.getWindow().getScaledWidth()) / 2;
+			int posY = (event.getWindow().getScaledHeight()) / 2;
+			PlayerEntity entity = Minecraft.getInstance().player;
+			World world = entity.world;
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			if (true) {
+				Minecraft.getInstance().fontRenderer.drawString(event.getMatrixStack(), "rehdbflhrfdbfjfbjfgffnkajrebfhjbrefdhsjdb", posX + -96,
+						posY + 63, -12829636);
+			}
+		}
+	}
+}
